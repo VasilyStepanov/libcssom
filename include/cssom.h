a07280f72ae04a6e3b281761a398a972bf8596fd
Vasily Stepanov <vasily.stepanov@gmail.com>
2012-02-09 12:25:44 +0400

    Stylesheet API inception.

--- include/cssom.h
+++ include/cssom.h
@@ -6,6 +6,48 @@ extern "C" {
 #endif
 
 
+typedef const char* CSSOM_DOMString;
+
+
+
+/**
+ * CSSRule.
+ */
+
+typedef struct _CSSOM_CSSRule CSSOM_CSSRule;
+
+unsigned short CSSOM_CSSRule_get_type(CSSOM_CSSRule *cssRule);
+
+
+
+/**
+ * CSSRuleList.
+ */
+
+typedef CSSOM_CSSRule* CSSOM_CSSRuleList;
+
+
+
+/**
+ * StyleSheet.
+ */
+
+typedef struct _CSSOM_StyleSheet CSSOM_StyleSheet;
+
+CSSOM_DOMString CSSOM_StyleSheet_get_type(CSSOM_StyleSheet *styleSheet);
+
+
+
+/**
+ * CSSStyleSheet.
+ */
+
+typedef struct _CSSOM_CSSStyleSheet CSSOM_CSSStyleSheet;
+
+const CSSOM_CSSRuleList*
+CSSOM_CSSStyleSheet_get_cssRules(CSSOM_CSSStyleSheet *styleSheet);
+
+
 
 #ifdef __cplusplus
 }
