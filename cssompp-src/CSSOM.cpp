7dae88fa9811d68cc1ba82bb62c4105371e26d00
Vasily Stepanov <vasily.stepanov@gmail.com>
2012-03-12 13:38:01 +0400

    CSSOM class.

--- cssompp-src/CSSOM.cpp
+++ cssompp-src/CSSOM.cpp
@@ -1,4 +1,32 @@
 #include <cssompp/CSSOM.hpp>
 
+#include "cssom-src/CSSOM.h"
+
+#include <new>
+
 namespace cssom {
+
+
+CSSOM::CSSOM(const char **properties) :
+  _impl(CSSOM_create(properties))
+{
+  if (_impl == NULL) throw std::bad_alloc();
+}
+
+
+
+CSSOM::CSSOM(const cssom::CSSOM &copy) :
+  _impl(copy._impl)
+{
+  CSSOM__acquire(_impl);
+}
+
+
+
+CSSOM::~CSSOM() {
+  CSSOM__release(_impl);
+}
+
+
+
 } // cssom
--- include/cssompp/CSSOM.hpp
+++ include/cssompp/CSSOM.hpp
@@ -1,11 +1,22 @@
 #ifndef CSSOMPP_CSSOM_HPP
 #define CSSOMPP_CSSOM_HPP
 
+#include <cssom/CSSOM.h>
+
 namespace cssom {
 
 
 
 class CSSOM {
+  public:
+    explicit CSSOM(const char **properties);
+    CSSOM(const cssom::CSSOM &copy);
+    ~CSSOM();
+
+    cssom::CSSOM& operator=(const cssom::CSSOM &rhs);
+
+  private:
+    ::CSSOM *_impl;
 };
 
 
