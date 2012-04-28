0cf3f6624e8a981802cc8c9535142577ca63eb57
Vasily Stepanov <vasily.stepanov@gmail.com>
2012-04-28 10:07:43 +0400

    Add dummy media list.

---
+++ tests/cssompp/test_MediaList.cpp
@@ -0,0 +1,23 @@
+#include "test_MediaList.hpp"
+
+#include "Errors.hpp"
+#include "utility.hpp"
+
+#include <assert.h>
+
+namespace {
+
+
+
+} // unnamed
+
+namespace test {
+
+
+
+void mediaList() {
+}
+
+
+
+} // test
---
+++ tests/cssompp/test_MediaList.hpp
@@ -0,0 +1,14 @@
+#ifndef TEST_MEDIA_LIST_HPP
+#define TEST_MEDIA_LIST_HPP
+
+namespace test {
+
+
+
+void mediaList();
+
+
+
+} // test
+
+#endif // TEST_MEDIA_LIST_HPP
